94a2e3141029663dd8715a0ba44cdd70e74ba2d2
danskeren <danskeren@ask.moe>
2019-09-23 19:22:19 +0200

    Updated dgo version

--- graph/graph.go
+++ graph/graph.go
@@ -8,10 +8,10 @@ import (
 type Graph interface {
 	Client() *dgo.Dgraph
 	SetSchema(schema string) error
-	SetJSON(data []byte) (*api.Assigned, error)
-	SetInterface(data interface{}) (*api.Assigned, error)
-	DeleteJSON(data []byte) (*api.Assigned, error)
-	DeleteInterface(data interface{}) (*api.Assigned, error)
+	SetJSON(data []byte) (*api.Response, error)
+	SetInterface(data interface{}) (*api.Response, error)
+	DeleteJSON(data []byte) (*api.Response, error)
+	DeleteInterface(data interface{}) (*api.Response, error)
 	Query(query string) (*api.Response, error)
 	QueryWithVars(query string, vars map[string]string) (*api.Response, error)
 	NodeValueExists(node, value string) bool
