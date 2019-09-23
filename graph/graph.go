066ee2bd7907f5f0155f914ecb1711d1d665512c
danskeren <danskeren@ask.moe>
2019-09-23 19:11:20 +0200

    Updated dgo version

--- graph/graph.go
+++ graph/graph.go
@@ -1,8 +1,8 @@
 package graph
 
 import (
-	"github.com/dgraph-io/dgo"
-	"github.com/dgraph-io/dgo/protos/api"
+	"github.com/dgraph-io/dgo/v2"
+	"github.com/dgraph-io/dgo/v2/protos/api"
 )
 
 type Graph interface {
